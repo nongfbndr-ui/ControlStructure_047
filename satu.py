dcb43df2d61c21c84f0e81f8f97decc5329dcaed
Moreno <nongfbndr@gmail.com>
2026-09-24 11:17:57 +0700

    pola angka

---
+++ satu.py
@@ -0,0 +1,12 @@
+persentase = int(input("Masukkan persentase nilai: "))
+
+if persentase >= 90:
+    print("Excellent performance")
+elif persentase >= 80:
+    print("Very Good performance")
+elif persentase >= 70:
+    print("Good performance")
+elif persentase >= 60:
+    print("Average performance")
+else:
+    print("Below average performance")
